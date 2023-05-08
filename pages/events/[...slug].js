d7dcf9b5eeed83b22b29a920e148d77e20f73ce5
jtahos <jtahos@yahoo.com>
2023-05-08 09:44:43 -0400

    Fix line endings project working up to section 78

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function FillteredEventsPage() {
+    return (
+    <div>
+        <h1>Filltered Events Page</h1>
+    </div>
+    );
+}
+
+export default FillteredEventsPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,10 @@
+function EventDetailPage() {
+    return (
+        <div>
+        <h1>Envent Detail Page</h1>
+    </div>
+    );
+    
+}
+
+export default EventDetailPage;
